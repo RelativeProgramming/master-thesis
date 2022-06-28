efe903c1d0ae08f07a2dc9fb1f494d74f3c91e57
RelativeProgramming <sebastian@dentalwendorf.de>
2022-06-28 07:55:19 +0200

    Updated jQA CLI

--- jqa-cli/bin/jqassistant.cmd
+++ jqa-cli/bin/jqassistant.cmd
@@ -1,3 +1,3 @@
 @echo off
 set JQASSISTANT_HOME=%~dp0%\..
-java %JQASSISTANT_OPTS% -jar "%JQASSISTANT_HOME%\lib\com.buschmais.jqassistant.cli-jqassistant-commandline-neo4jv3-1.11.1.jar" %*
+java %JQASSISTANT_OPTS% -jar "%JQASSISTANT_HOME%\lib\com.buschmais.jqassistant.cli-jqassistant-commandline-neo4jv3-1.12.0-M1.jar" %*
--- jqa-cli/bin/jqassistant.sh
+++ jqa-cli/bin/jqassistant.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 BIN_DIR=`dirname "$0"`
 export JQASSISTANT_HOME=`cd "$BIN_DIR/.." && pwd -P`
-java $JQASSISTANT_OPTS -jar "$JQASSISTANT_HOME/lib/com.buschmais.jqassistant.cli-jqassistant-commandline-neo4jv3-1.11.1.jar" "$@"
+java $JQASSISTANT_OPTS -jar "$JQASSISTANT_HOME/lib/com.buschmais.jqassistant.cli-jqassistant-commandline-neo4jv3-1.12.0-M1.jar" "$@"
